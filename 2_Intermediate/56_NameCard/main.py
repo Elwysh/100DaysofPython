16026df714ef55c40a9024e6276c9469c40200c3
Elwysh <elwysh.code@gmail.com>
2024-08-28 15:35:21 +0200

    added folder files to X template "finished day 56"

---
+++ 2_Intermediate/56_NameCard/main.py
@@ -0,0 +1,5 @@
+# Task description - customize your profile website from the website in the course
+# same as hello.py regarding main.py solution but index.html is heavily customized
+# may update later
+
+print("Hello world.")
